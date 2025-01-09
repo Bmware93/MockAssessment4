68012ac9008c569bc0886d647868e962b420063b
Benia Morgan <bmorganware@gmail.com>
2025-01-09 10:12:12 -0500

    created project. First call class created

---
+++ .vscode/Call.java
@@ -0,0 +1,15 @@
+public class Call {
+    private int date;
+    private String notes;
+    public int getDate() {
+        return date;
+    }
+    public String getNotes() {
+        return notes;
+    }
+    public Call(int date, String notes) {
+        this.date = date;
+        this.notes = notes;
+    }
+    
+}
